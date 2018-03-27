803549707509ab559f01d9c052b01ba6128b2efc
Alex <alex@insidegadgets.com>
2018-03-27 17:50:12 +1000

    PCB v1.1 / v1.2 (Firmware R9) – 19 March 2018 - Re-written Gameboy flash cart writing functions - Added support for SST39SF010A & AT49F040 – 32K Gameboy Flash Carts [Thanks to Jonas S. & Wes for reporting/testing]
    Console Flasher
    v1.7 – 19 March 2018
    - Added support for SST39SF010A & AT49F040 – 32K Gameboy Flash Carts [Thanks to Jonas S. & Wes for reporting/testing]
    
    Console Interface
    v1.11 – 19 March 2018
    - Added a more thorough check for GBA SRAM saves
    - Added support for SST39SF010A & AT49F040 – 32K Gameboy Flash Carts (Firmware update R9 required) [Thanks to Jonas S. & Wes for reporting/testing]
    
    GUI Interface
    v1.16 – 19 March 2018
    - Added a more thorough check for GBA SRAM saves
    - Added support for SST39SF010A & AT49F040 – 32K Gameboy Flash Carts (Firmware update R9 required) [Thanks to Jonas S. & Wes for reporting/testing]

--- MCU_Files/Program-Bootloader.bat
+++ MCU_Files/Program-Bootloader.bat
@@ -2,6 +2,6 @@
 
 avrdude -p atmega8515 -c usbasp -U lfuse:w:0xaf:m -U hfuse:w:0xda:m
 
-avrdude -p atmega8515 -c usbasp -U flash:w:GBxCart_RW_v1.1_R8\tsb\tsb_m8515_d0d1_resetwdt.hex
+avrdude -p atmega8515 -c usbasp -U flash:w:GBxCart_RW_v1.1_R9\tsb\tsb_m8515_d0d1_resetwdt.hex
 
 pause
