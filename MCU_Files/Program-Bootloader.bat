7e5bbcb3a38f7f64eda678c607a31be0e106f624
Alex <alex@insidegadgets.com>
2019-12-21 11:50:24 +1000

    21 Dec 2019 – GUI v1.33 – Changed back GBA ROM reading from 256 byte reads to 64 byte reads as some users experienced “retrying” issues – Added GBxMAS RW support

--- MCU_Files/Program-Bootloader.bat
+++ MCU_Files/Program-Bootloader.bat
@@ -2,6 +2,6 @@
 
 avrdude -p atmega8515 -c usbasp -U lfuse:w:0xaf:m -U hfuse:w:0xda:m
 
-avrdude -p atmega8515 -c usbasp -U flash:w:GBxCart_RW_v1.1-1.2_R14\tsb\tsb_m8515_d0d1_resetwdt.hex
+avrdude -p atmega8515 -c usbasp -U flash:w:GBxCart_RW_v1.1-1.2_R15\tsb\tsb_m8515_d0d1_resetwdt.hex
 
 pause
